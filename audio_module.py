26aeb392c8fef7cc85ca8f48f6eeaabaa8e68695
jtblackk <jeffblackbusiness@gmail.com>
2020-07-16 13:34:55 -0400

    added description for audio module

---
+++ audio_module.py
@@ -0,0 +1,7 @@
+# this program is the audio capturer and emitter
+# part of Unify. It's purpose is to capture audio
+# from the system running the program, as well as
+# emit audio to the speakers of the system running
+# the program.
+
+
