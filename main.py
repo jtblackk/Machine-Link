1fe55d06204cb558ed97bd9afdc16f2c92100e44
jtblackk <jeffblackbusiness@gmail.com>
2020-07-20 20:45:19 -0400

    added window icon

--- main.py
+++ main.py
@@ -6,5 +6,6 @@ from gui_obj import *
 if __name__ == "__main__":
     master = tk.Tk()
     master.title("Unify")
+    master.iconbitmap('assets/unify_icon_x7I_icon.ico')
     window = GUI(master)
     master.mainloop()
