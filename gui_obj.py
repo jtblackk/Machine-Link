43ed4d966e0a999562fd675baf3c6d54588247a3
jtblackk <jtblack@g.clemson.edu>
2022-12-31 01:25:25 -0500

    Fixed errors when stopping connections

--- gui_obj.py
+++ gui_obj.py
@@ -23,116 +23,115 @@ class GUI:
         # ------ RECEIVE AUDIO SECTION ------ #
         # receiver section title
         self.receive_title = tk.Label(
-                            master = self.master, 
-                            text = "Receive Audio")
-        self.receive_title.grid(column = 0, row = 0, columnspan = 2)
+            master=self.master,
+            text="Receive Audio")
+        self.receive_title.grid(column=0, row=0, columnspan=2)
 
         # sender ip address form
         self.sender_ip_label = tk.Label(
-                                    master = self.master, 
-                                    text = "Sender IP:")
-        self.sender_ip_label.grid(column = 0, row = 1, sticky = tk.E)
+            master=self.master,
+            text="Sender IP:")
+        self.sender_ip_label.grid(column=0, row=1, sticky=tk.E)
         self.sender_ip_entry = tk.Entry(
-                                master = self.master,
-                                width = 14)
-        self.sender_ip_entry.grid(column = 1, row = 1, sticky = tk.W)
+            master=self.master,
+            width=14)
+        self.sender_ip_entry.grid(column=1, row=1, sticky=tk.W)
 
         # sender port form
         self.sender_port_label = tk.Label(
-                                    master = self.master, 
-                                    text = "Sender Port:")
-        self.sender_port_label.grid(column = 0, row = 2, sticky = tk.E)
+            master=self.master,
+            text="Sender Port:")
+        self.sender_port_label.grid(column=0, row=2, sticky=tk.E)
         self.sender_port_entry = tk.Entry(
-                                master = self.master,
-                                width = 4)
-        self.sender_port_entry.grid(column = 1, row = 2, sticky = tk.W)
+            master=self.master,
+            width=4)
+        self.sender_port_entry.grid(column=1, row=2, sticky=tk.W)
 
         # start receiving button
         self.receive_start_button = tk.Button(
-                                    master = self.master, 
-                                    text = "Start",
-                                    width = 5, 
-                                    command = self.threaded_start_receiver)
-        self.receive_start_button.grid(column = 1, row = 3, sticky = tk.W)
+            master=self.master,
+            text="Start",
+            width=5,
+            command=self.threaded_start_receiver)
+        self.receive_start_button.grid(column=1, row=3, sticky=tk.W)
 
         # stop receiving button
         self.receive_stop_button = tk.Button(
-                                    master = self.master, 
-                                    text = "Stop",
-                                    width = 5, 
-                                    state = tk.DISABLED, 
-                                    command = self.threaded_stop_receiver)
-        self.receive_stop_button.grid(column = 1, row = 4, sticky = tk.W)
+            master=self.master,
+            text="Stop",
+            width=5,
+            state=tk.DISABLED,
+            command=self.threaded_stop_receiver)
+        self.receive_stop_button.grid(column=1, row=4, sticky=tk.W)
 
         # receiver status
         self.receiver_status_label = tk.Label(
-                                    master = self.master, 
-                                    text = "Status:")
-        self.receiver_status_label.grid(column = 0, row = 5, sticky = tk.E)
+            master=self.master,
+            text="Status:")
+        self.receiver_status_label.grid(column=0, row=5, sticky=tk.E)
         self.receiver_status = tk.Label(
-                                master = self.master, 
-                                text = "Off")
-        self.receiver_status.grid(column = 1, row = 5, sticky = tk.W)
-
+            master=self.master,
+            text="Off")
+        self.receiver_status.grid(column=1, row=5, sticky=tk.W)
 
         # ------ SEND AUDIO SECTION ------ #
         # sender section title
         self.send_title = tk.Label(
-                            master = self.master, 
-                            text = "Send Audio")
-        self.send_title.grid(column = 2, row = 0, columnspan = 2)
+            master=self.master,
+            text="Send Audio")
+        self.send_title.grid(column=2, row=0, columnspan=2)
 
         # audio device form
         self.audio_device_label = tk.Label(
-                                    master = self.master, 
-                                    text = "Stream from:")
-        self.audio_device_label.grid(column = 2, row = 1, sticky = tk.E)
+            master=self.master,
+            text="Stream from:")
+        self.audio_device_label.grid(column=2, row=1, sticky=tk.E)
         self.audio_device_selection = tk.OptionMenu(
-                                            self.master, 
-                                            self.audio_source, 
-                                            self.audio_source.get(), 
-                                            *(self.sender_module.get_audio_devices()))
-        self.audio_device_selection.grid(column = 3, row = 1)
+            self.master,
+            self.audio_source,
+            self.audio_source.get(),
+            *(self.sender_module.get_audio_devices()))
+        self.audio_device_selection.grid(column=3, row=1)
 
         # start stream button
         self.send_start_button = tk.Button(
-                                    master = self.master, 
-                                    text = "Start", 
-                                    width = 5,
-                                    command = self.threaded_start_sender)
-        self.send_start_button.grid(column = 3, row = 3, sticky = tk.W)
+            master=self.master,
+            text="Start",
+            width=5,
+            command=self.threaded_start_sender)
+        self.send_start_button.grid(column=3, row=3, sticky=tk.W)
 
         # stop stream button
         self.send_stop_button = tk.Button(
-                                master = self.master, 
-                                text = "Stop", 
-                                width = 5,
-                                state = tk.DISABLED, 
-                                command = self.threaded_stop_sender)
-        self.send_stop_button.grid(column = 3, row = 4, sticky = tk.W)
+            master=self.master,
+            text="Stop",
+            width=5,
+            state=tk.DISABLED,
+            command=self.threaded_stop_sender)
+        self.send_stop_button.grid(column=3, row=4, sticky=tk.W)
 
         # sender status
         self.sender_status_label = tk.Label(
-                                    master = self.master, 
-                                    text = "Status:")
-        self.sender_status_label.grid(column = 2, row = 5, sticky = tk.E)
+            master=self.master,
+            text="Status:")
+        self.sender_status_label.grid(column=2, row=5, sticky=tk.E)
         self.sender_status_text = tk.Label(
-                            master = self.master, 
-                            text = "Off")
-        self.sender_status_text.grid(column = 3, row = 5, sticky = tk.W)
+            master=self.master,
+            text="Off")
+        self.sender_status_text.grid(column=3, row=5, sticky=tk.W)
 
         # stream address
         self.sender_address_label = tk.Label(
-                                    master = self.master, 
-                                    text = "Sender Address:")
-        self.sender_address_label.grid(column = 2, row = 6, sticky = tk.E)
+            master=self.master,
+            text="Sender Address:")
+        self.sender_address_label.grid(column=2, row=6, sticky=tk.E)
         self.sender_address_text = tk.Label(
-                            master = self.master, 
-                            text = "Unavailable")
-        self.sender_address_text.grid(column = 3, row = 6, sticky = tk.W)
-
+            master=self.master,
+            text="Unavailable")
+        self.sender_address_text.grid(column=3, row=6, sticky=tk.W)
 
     # threads the start_receiver callback
+
     def threaded_start_receiver(self):
         receiver_thread = td.Thread(target=self.start_receiver)
         receiver_thread.setDaemon(True)
@@ -148,25 +147,27 @@ class GUI:
             self.receiver_status['text'] = "Invalid address"
             self.receiver_status['fg'] = "red"
             return
-        
+
         # validate port entry
         if not self.sender_port_entry.get().isdigit():
             self.receiver_status['text'] = "Invalid Port"
             self.receiver_status['fg'] = "red"
 
-
         # establish connection with sender
         self.receiver_status['text'] = "Connecting"
         self.receiver_status['fg'] = "orange"
         try:
-            self.receiver_module.connect_to_sender(self.sender_ip_entry.get(), int(self.sender_port_entry.get()))
-        except: # provide status update if connection couldn't be established
+            self.receiver_module.connect_to_sender(
+                self.sender_ip_entry.get(), int(self.sender_port_entry.get()))
+        except:  # provide status update if connection couldn't be established
+            print()
             self.receiver_status['text'] = "Connection failed"
             self.receiver_status['fg'] = "red"
             return
+
         self.receiver_status['text'] = "Connected"
         self.receiver_status['fg'] = "green"
-        
+
         # switch start/stop button states
         self.receive_start_button['state'] = tk.DISABLED
         self.receive_stop_button['state'] = tk.ACTIVE
@@ -209,7 +210,7 @@ class GUI:
     # callback function for when the user presses "start" on the sender module
     def start_sender(self):
         # create a socket
-        try: 
+        try:
             self.sender_module.create_socket()
             self.send_start_button['state'] = tk.DISABLED
             self.send_stop_button['state'] = tk.ACTIVE
@@ -218,7 +219,7 @@ class GUI:
             self.sender_address_text['text'] = f"{self.sender_module.sender_address} @ port {self.sender_module.sender_port}"
         except:
             self.sender_status_text['text'] = "Session creation failed"
-            self.sender_status_text['fg'] = "red"    
+            self.sender_status_text['fg'] = "red"
             return
 
         # connect to the receiver
@@ -231,11 +232,13 @@ class GUI:
 
         # if connection closes, handle it
         self.threaded_stop_sender()
-        self.sender_module.audio_stream.stop_stream()
+        if(hasattr(self.sender_module, "audio_stream")):
+            self.sender_module.audio_stream.stop_stream()
         self.sender_module.receiver_socket.close()
 
     # threads the stop_sender callback
     def threaded_stop_sender(self):
+        self.sender_address_text['text'] = "Unavailable"
         stop_sender_thread = td.Thread(target=self.stop_sender)
         stop_sender_thread.setDaemon(True)
         self.threads.append(stop_sender_thread)
@@ -243,7 +246,8 @@ class GUI:
 
     # callback function for when the user presses "stop" on the sender module
     def stop_sender(self):
-        self.sender_module.audio_stream.stop_stream()
+        if(hasattr(self.sender_module, "audio_stream")):
+            self.sender_module.audio_stream.stop_stream()
         self.sender_module.sender_socket.close()
         self.sender_status_text['text'] = "Disconnected"
         self.sender_status_text['fg'] = "black"
--- receiver_obj.py
+++ receiver_obj.py
@@ -1,7 +1,7 @@
 # this program is the receiver part of Unify.
 # its purpose is to receive audio that has
 # been transmitted by the sender.py counterpart
-# to this program. 
+# to this program.
 
 
 import socket
@@ -14,8 +14,8 @@ class receiver:
     RATE = 44100
     p = pyaudio.PyAudio()
 
-
     # establish a socket connection with the sender module
+
     def connect_to_sender(self, sender_address, sender_port):
         # create a new socket
         self.sender_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -29,24 +29,21 @@ class receiver:
         num_channels = self.sender_socket.recv(self.CHUNK_SIZE).decode('utf-8')
 
         # open audio stream
-        self.audio_stream = self.p.open(format=self.FORMAT, 
-                        channels=int(num_channels), 
-                        rate=self.RATE,
-                        output=True)
+        self.audio_stream = self.p.open(format=self.FORMAT,
+                                        channels=int(num_channels),
+                                        rate=self.RATE,
+                                        output=True)
 
         # connection loop
         while True:
             try:
                 # receive data from sender module
                 received_data = self.sender_socket.recv(self.CHUNK_SIZE)
-                
+
                 if not received_data:
                     break
 
                 # display the data (emit audio)
                 self.audio_stream.write(received_data)
-            except: # break if there's an interruption in the data flow
+            except:  # break if there's an interruption in the data flow
                 break
-
-
-    
